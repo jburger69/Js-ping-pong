03a07a9f88ef68fc39e6abbde9e2fc285c5977fd
Joshua Burger <joshberger6969@outlook.com>
2022-03-26 23:14:26 -0400

    Create canvas and function to show canvas

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+var canvas;
+var canvasContext;
+var ballX = 50;
+
+
+
 window.onload = function() {
     console.log("Hello World!")
+    canvas = document.getElementById('gameCanvas');
+    canvasContext = canvas.getContext('2d');
+    setInterval(drawEverything, 100);
+}
+
+function drawEverything() {
+    ballX = ballX + 20;
+
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillRect(0,0,canvas.width,canvas.height);
+    canvasContext.fillStyle = 'white';
+    canvasContext.fillRect(225,210,200,200);
+    canvasContext.fillStyle = 'red';
+    canvasContext.fillRect(ballX, 100, 10, 10);
+
 }
